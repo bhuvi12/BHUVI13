96b0d7a6cd90a20514a30347022919984cb43799
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-01-31 16:09:54 +0530

    odd interval.C

---
+++ ODD NUMINTERVAL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,m=1,n=6;
+clrscr();
+printf("print odd number ");
+for(num=m;num<=n;num++)
+{
+if(num%2==1)
+printf("%d",num);
+}
+getch();
+}
